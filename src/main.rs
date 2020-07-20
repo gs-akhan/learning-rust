d933ed20b74d3b2d16ab36747d5f386f67146ec3
Azhar Uddin <akhan@gainsight.com>
2020-07-21 02:16:19 +0530

    Added read from command line.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    
+    println!("Welcome to Rust Guessing Game !!");
+
+    println!("Please enter your Guess"); 
+    
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed : {}", guess);
 }
