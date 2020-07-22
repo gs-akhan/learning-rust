f8f9393acd0d9bdc9f5b53e91d71547e117240d9
Azhar Uddin <akhan@gainsight.com>
2020-07-23 02:47:12 +0530

    Added comparison.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate rand;
 
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     
@@ -17,6 +18,12 @@ fn main() {
     println!("You guessed : {}", guess);
 
     let secret_number = rand::thread_rng().gen_range(1,100);
-    
-    println!("{}", secret_number);
+
+    println!("Secret is {} ", secret_number);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!("You wil"),
+    }
 }
