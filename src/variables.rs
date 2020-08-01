dfc1055aad163e5078d9707ae441b4fb8baab0ef
Azhar Uddin <akhan@gainsight.com>
2020-08-02 04:30:08 +0530

    Added constant

--- src/variables.rs
+++ src/variables.rs
@@ -4,4 +4,17 @@ fn main() {
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+
+    //shadowing 
+
+    let x = 100;
+    let x = 100 + 1;
+    let x = x * 2;
+
+    //This way of declaring new variables with same name as previous variables
+    //is called as shadowing. 
+    //Specially used in place where we want to use same name 
+    //for variables after assigning them different types.
+
+
 }
