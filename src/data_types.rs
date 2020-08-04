cb44500b248fc724f824450223b8009beef3c2b5
Azhar Uddin <akhan@gainsight.com>
2020-08-05 04:34:36 +0530

    Added constant

--- src/data_types.rs
+++ src/data_types.rs
@@ -1,1 +1,11 @@
 // Here we will learn about types.
+
+// A scalar type represents a single value. Rust has four primary scalar
+// types: integers, floating-point numbers, booleans, and characters. You’ll
+// likely recognize these from other programming languages, but let’s
+// jump into how they work in Rust.
+
+fn main() {
+    let age : i32 = 29;
+    println!("{}", age);
+}
