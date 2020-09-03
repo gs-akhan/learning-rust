80ea8297541eeaf4dd400f2610599c6b4bc55eb2
Azhar Uddin <akhan@gainsight.com>
2020-09-04 03:46:00 +0530

    Added first ever struct

--- src/structs.rs
+++ src/structs.rs
@@ -1,4 +1,22 @@
 fn main() {
     let name = String::from("We are in Structs");
     println!("{}", name);
+    struct User {
+        user_name: String,
+        email: String,
+        age: i32,
+        active: bool,
+    };
+
+    let azhar = User {
+        user_name: String::from("Azhar"),
+        email: String::from("wolf@crazyman.com"),
+        age: 29,
+        active: true,
+    };
+
+    println!("Name is : {}", azhar.user_name);
+    println!("Age is : {}", azhar.age);
+    println!("email is : {}", azhar.email);
+    println!("Active is : {}", azhar.active);
 }
