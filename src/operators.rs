6b7aa65641d5c50fa1c7c318c08f4b6bca755cce
Azhar Uddin <akhan@gainsight.com>
2020-09-05 22:41:37 +0530

    Added first ever operator

---
+++ src/operators.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello Operators");
+
+    let mut array:Vec<i32> = vec![1,2,3,4,5,6,7];
+
+    array.push(8);
+    array.iter().filter(|x| **x > 4)
+        .for_each(|x| println!("{}", x));
+
+}
