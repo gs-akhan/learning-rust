3418e138e14a03d766361086ada718ac936fb8a9
Azhar Uddin <akhan@gainsight.com>
2020-09-05 22:43:14 +0530

    Added map operator

--- src/operators.rs
+++ src/operators.rs
@@ -4,7 +4,10 @@ fn main() {
     let mut array:Vec<i32> = vec![1,2,3,4,5,6,7];
 
     array.push(8);
-    array.iter().filter(|x| **x > 4)
+    array.iter()
+        .map(|x| x * 2)
+        .filter(|x| *x > 4)
         .for_each(|x| println!("{}", x));
 
+    
 }
