8409ff23a87fcf29f0668510ae70db303062d66e
Azhar Uddin <akhan@gainsight.com>
2020-09-30 22:56:20 +0530

    Added formatting text

--- src/map.rs
+++ src/map.rs
@@ -1,11 +1,9 @@
 fn main() {
-    println!("{}", "Learning Map");
-
+    println!("{}", "==== Learning Map ====");
     let names = vec!["azhar", "khan"];
-
-    let upp_names:Vec<String> = names.iter().map(|x| x.to_uppercase()).collect();
+    let upp_names: Vec<String> = names.iter().map(|x| x.to_uppercase()).collect();
 
     upp_names.iter().for_each(|x| println!("{}", x));
 
-    
+
 }
