6ddfd06746e2c286e3feaf650a1d13d383839ca3
Azhar Uddin <akhan@gainsight.com>
2020-10-08 23:43:56 +0530

    Added Prime Factors Improved logic

--- src/primefactors_new.rs
+++ src/primefactors_new.rs
@@ -1,3 +1,19 @@
 fn main() {
     println!("Improved Prime factors");
+
+    let mut value = 8;
+    let mut result = vec![];
+
+    for factor in 2.. {
+        if value < 2 {
+            break;
+        }
+
+        while value % factor == 0 {
+            value = value / factor;
+            result.push(factor);
+        }
+    }
+
+    result.iter().for_each(|x| println!("{}", x));
 }
