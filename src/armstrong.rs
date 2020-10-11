6c7ff027ebb1273d3ac52ea728fb0339849a6052
Azhar Uddin <akhan@gainsight.com>
2020-10-11 19:49:38 +0530

    Added armstrong logic

--- src/armstrong.rs
+++ src/armstrong.rs
@@ -1,5 +1,19 @@
 fn main() {
     println!("{}", "==== Armstrong Numbers ====");
+
+    let num = "153";
+
+    let str = num.to_string();
+
+    let count = 3;
+    let mut result = 0;
+    for (_, c) in str.chars().enumerate() {
+        let num: i64 = c.to_string().parse().expect("Failed to convert");
+        println!("{}", num);
+        result = result + num.pow(count);
+    }
+
+    println!("{}", result);
 }
 
 pub fn is_armstrong() -> bool {
