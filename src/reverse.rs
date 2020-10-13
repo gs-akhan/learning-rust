87118842f53594eab3e9af83ea2051b58769cdea
Azhar Uddin <akhan@gainsight.com>
2020-10-13 20:41:34 +0530

    Added Reverse String logic

--- src/reverse.rs
+++ src/reverse.rs
@@ -1,3 +1,9 @@
 fn main() {
     println!("{}", "Reversing the String ");
+    let str = "Azhar is here";
+    let mut result = vec![];
+    for (_, c) in str.chars().enumerate() {
+        result.insert(0, c.to_string());
+    }
+    println!("{}", result.join(""));
 }
