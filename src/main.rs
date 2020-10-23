799d0409573d7215eb2e0cb4bb26c0e8ff3bb052
Azhar Uddin <akhan@gainsight.com>
2020-10-23 23:49:44 +0530

    Added Rocket web server and hello word API 🚀

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,48 @@
-extern crate rand;
-extern crate reqwest;
-use chrono::prelude::*;
+#![feature(proc_macro_hygiene, decl_macro)]
 
 // use rand::Rng;
 // use std::cmp::Ordering;
 // use std::io;
+use rocket::*;
 
 fn main() {
     println!("Welcome to Rust Guessing Game !!");
-
-    // let value: BigUint = 1.to_bigint().unwrap();
-
-    // loop {
-    //     println!("Please enter your Guess");
-
-    //     let mut guess = String::new();
-
-    //     io::stdin()
-    //         .read_line(&mut guess)
-    //         .expect("Failed to read line");
-
-    //     let guess: u32 = match guess.trim().parse() {
-    //         Ok(num) => num,
-    //         Err(_) => continue,
-    //     };
-
-    //     println!("You guessed : {}", guess);
-
-    //     let secret_number = rand::thread_rng().gen_range(1, 100);
-
-    //     println!("Secret is {} ", secret_number);
-
-    //     match guess.cmp(&secret_number) {
-    //         Ordering::Less => println!("Too small"),
-    //         Ordering::Greater => println!("Too big"),
-    //         Ordering::Equal => {
-    //             println!("You win");
-    //             break;
-    //         }
-    //     }
-    // }
+    rocket::ignite().mount("/", routes![hello]).launch();
 }
+
+#[get("/hello")]
+fn hello() -> String {
+    format!("Hello, This is my first API Rust!")
+}
+
+// let value: BigUint = 1.to_bigint().unwrap();
+
+// loop {
+//     println!("Please enter your Guess");
+
+//     let mut guess = String::new();
+
+//     io::stdin()
+//         .read_line(&mut guess)
+//         .expect("Failed to read line");
+
+//     let guess: u32 = match guess.trim().parse() {
+//         Ok(num) => num,
+//         Err(_) => continue,
+//     };
+
+//     println!("You guessed : {}", guess);
+
+//     let secret_number = rand::thread_rng().gen_range(1, 100);
+
+//     println!("Secret is {} ", secret_number);
+
+//     match guess.cmp(&secret_number) {
+//         Ordering::Less => println!("Too small"),
+//         Ordering::Greater => println!("Too big"),
+//         Ordering::Equal => {
+//             println!("You win");
+//             break;
+//         }
+//     }
+// }
