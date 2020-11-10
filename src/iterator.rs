7c87e3cb881804791e54b87d2f5f42685e7042db
Azhar Uddin <akhan@gainsight.com>
2020-11-10 22:47:33 +0530

    Added iterator

--- src/iterator.rs
+++ src/iterator.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("{}", "Learning Iterators");
+    let languages = vec!["JS", "Java", "Perl", "Erlang", "Go", "Rust"];
+
+    for (_pos, val) in languages.iter().enumerate() {
+        println!("{}", val);
+    }
+
+    println!("{}", "Printin using Map");
+    languages.iter().for_each(|x| println!("{}", x));
+    
+
 }
