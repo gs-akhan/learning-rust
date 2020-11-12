428bc15f75d4d8d5cb4df8c1811d07965ecbe783
Azhar Uddin <akhan@gainsight.com>
2020-11-13 03:17:08 +0530

    Learning Strings

--- src/string.rs
+++ src/string.rs
@@ -1,3 +1,24 @@
 fn main() {
     println!("Working with the Strings");
+    
+    let my_name = "Azhar is HERE";
+
+    println!("Lower Cased : {}", my_name.to_lowercase());
+
+    let newname = my_name;
+
+    println!("Orig String {}", newname);
+    println!("Orig String {}", my_name);
+
+
+    // Let String as a Objet
+
+    let hello = String::from("Hello MR ");
+
+
+    // Pass my Reference !!
+    let new_hello = &hello;
+
+    println!("{}", hello);
+    
 }
