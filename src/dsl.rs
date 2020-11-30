878cfda13aaae4ba985ef9a04ed35cf3c58b3093
Azhar Uddin <akhan@gainsight.com>
2020-11-30 22:35:43 +0530

    Added DSL empty file

---
+++ src/dsl.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Learning DSL ");
+
+    let code = "graph {
+        graph [bgcolor="yellow"]
+        a [color="red"]
+        b [color="blue"]
+        a -- b [color="green"]
+    }";
+}
