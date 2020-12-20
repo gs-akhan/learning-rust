6a36ba7774a6de941dfa2db6bd9fac988a8559aa
Azhar Uddin <akhan@gainsight.com>
2020-12-21 04:18:43 +0530

    Experimenting more on the async callouts

--- src/request.rs
+++ src/request.rs
@@ -1,6 +1,35 @@
+#[macro_use]
+extern crate serde_derive;
+
+use futures::future;
 use log::*;
 
-fn main() {
+#[derive(Serialize, Deserialize, Debug)]
+struct Ip {
+    origin: String,
+}
+#[tokio::main]
+async fn main() {
     println!("Azhar is here");
     info!("New position: x: {}, y: {}", 1, 2);
+
+    let rt = tokio::runtime::Runtime::new().unwrap();
+
+    rt.enter(|| {
+        println!("in r.enter()");
+        tokio::spawn(future::lazy(|_| {
+            println!("WE are in the spwan");
+        }));
+    });
+    get_data().await;
+}
+
+async fn get_data() {
+    let body = reqwest::get("https://www.google.com")
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+    println!("{:?}", body);
 }
