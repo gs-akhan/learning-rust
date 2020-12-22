a20ee63152a122eea05f59b679dde8a3138a962f
Azhar Uddin <akhan@gainsight.com>
2020-12-23 05:10:50 +0530

    Text REsponse for HTTP calls

--- src/request.rs
+++ src/request.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate serde_derive;
 
+use futures::executor::block_on;
 use futures::future;
 use log::*;
 
@@ -18,6 +19,8 @@ struct Books {
     data: Vec<Book>,
 }
 
+use reqwest::Response;
+
 #[tokio::main]
 async fn main() {
     println!("Azhar is here");
@@ -31,23 +34,26 @@ async fn main() {
             println!("WE are in the spwan");
         }));
     });
-    // get_data().await;
-    let resp = get_json().await;
-    match resp {
-        Err(e) => println!("Error {}", e),
-        // Ok(d) => println!("Printing Name of Book {:?}", d.data[0].name),
-        Ok(d) => println!("Printing Entire JSON : {:?}", d),
+    let data = get_data().await;
+    match data {
+        Err(_e) => println!("We have error"),
+        Ok(d) => {
+            let data = d.text().await;
+            println!("{:?}", data);
+        }
     }
+
+    // let resp = get_json().await;
+    // match resp {
+    //     Err(e) => println!("Error {}", e),
+    //     // Ok(d) => println!("Printing Name of Book {:?}", d.data[0].name),
+    //     Ok(d) => println!("Printing Entire JSON : {:?}", d),
+    // }
 }
 
-async fn get_data() {
-    let body = reqwest::get("https://www.google.com")
-        .await
-        .unwrap()
-        .text()
-        .await
-        .unwrap();
-    println!("{:?}", body);
+async fn get_data() -> Result<Response, Box<std::error::Error>> {
+    let body = reqwest::get("https://www.google.com").await?;
+    Ok(body)
 }
 
 async fn get_json() -> Result<Books, Box<dyn std::error::Error>> {
