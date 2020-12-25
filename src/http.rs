028e5ee19a726374fbc56c7f2f48a05420c14e9f
Azhar Uddin <akhan@gainsight.com>
2020-12-26 02:16:12 +0530

    Added http file

---
+++ src/http.rs
@@ -0,0 +1,31 @@
+use reqwest::*;
+#[macro_use]
+extern crate serde_derive;
+use reqwest::Result;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Book {
+    name: String,
+    imageurl: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Books {
+    data: Vec<Book>,
+}
+
+#[tokio::main]
+async fn main() {
+    println!("This is http file");
+
+    let data = get_json().await;
+    println!("{:?}", data);
+}
+
+async fn get_json() -> Result<Books> {
+    let json: Books = reqwest::get("https://pokemon.proxy.beeceptor.com/books")
+        .await?
+        .json::<Books>()
+        .await?;
+    Ok(json)
+}
